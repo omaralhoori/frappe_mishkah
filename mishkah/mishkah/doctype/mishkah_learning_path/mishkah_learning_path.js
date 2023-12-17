ddf74e03867713fd7ec09018930f1be391e5c9a7
Omar <omaralhoori@gmail.com>
2023-12-17 14:41:26 +0300

    learing path dont use course again

--- mishkah/mishkah/doctype/mishkah_learning_path/mishkah_learning_path.js
+++ mishkah/mishkah/doctype/mishkah_learning_path/mishkah_learning_path.js
@@ -6,3 +6,16 @@
 
 // 	},
 // });
+
+
+frappe.ui.form.on('Mishkah Learning Path Stage', {
+	stages_add: function(frm){
+		frm.fields_dict['stages'].grid.get_field('course').get_query = function(doc){
+			var courses_list = [];
+			$.each(doc.stages, function(idx, val){
+				if (val.course) courses_list.push(val.course);
+			});
+			return { filters: [['Mishkah Course', 'name', 'not in', courses_list]] };
+		};
+	}
+});
