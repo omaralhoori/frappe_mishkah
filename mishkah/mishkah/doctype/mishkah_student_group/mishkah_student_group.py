d4eaf12787b2c5a0455b7c8c0cdefded0471d63d
Omar <omaralhoori@gmail.com>
2024-06-12 08:39:56 +0200

    edit insturctor

--- mishkah/mishkah/doctype/mishkah_student_group/mishkah_student_group.py
+++ mishkah/mishkah/doctype/mishkah_student_group/mishkah_student_group.py
@@ -11,6 +11,25 @@ class MishkahStudentGroup(Document):
 				self.students.remove(s)
 				return True
 			
+	def validate(self):
+		self.validate_group_order()
+	def validate_group_order(self):
+		if self.parent_mishkah_student_group:
+			parent_group_type = frappe.db.get_value("Mishkah Student Group", self.parent_mishkah_student_group, "group_type")
+			if self.group_type == parent_group_type:
+				frappe.throw("Group type cannot be same as the parent group")
+			order_false = False
+			if parent_group_type == 'Student Subgroup' and self.group_type in ('Program Group', 'Level Group', 'Student Main Group', 'Student Group'):
+				order_false = True
+			if parent_group_type == 'Student Group' and self.group_type in ('Program Group', 'Level Group', 'Student Main Group'):
+				order_false = True
+			if parent_group_type == 'Student Main Group' and self.group_type in ('Program Group', 'Level Group'):
+				order_false = True
+			if parent_group_type == 'Level Group' and self.group_type =='Program Group':
+				order_false = True
+			if order_false:
+				frappe.throw("There is an error in the order of the groups")
+			
 	def on_update(self):
 		self.students_count = len(self.students)
 		self.db_set("students_count", self.students_count)
