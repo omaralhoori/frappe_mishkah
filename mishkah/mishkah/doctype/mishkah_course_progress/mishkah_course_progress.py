2b4df491bce7e8c679ed5db3b3beb9001371ec9c
Omar <omaralhoori@gmail.com>
2024-12-10 21:34:51 +0100

    enrollment updated, and certififcate

--- mishkah/mishkah/doctype/mishkah_course_progress/mishkah_course_progress.py
+++ mishkah/mishkah/doctype/mishkah_course_progress/mishkah_course_progress.py
@@ -6,7 +6,6 @@ from frappe.model.document import Document
 
 class MishkahCourseProgress(Document):
 	def on_update(self):
-		return
 		total = frappe.db.sql("""
 			SELECT SUM(points) as total
 			FROM `tabMishkah Course Progress`
@@ -16,7 +15,10 @@ class MishkahCourseProgress(Document):
 		frappe.db.set_value("Mishkah Level Enrollment", self.level_enrollment, {"total_level_points": total_points})
 		
 
-def update_level_enrollment_points(level_enrollment=None):
+def update_level_enrollment_points(level_enrollment=None, level=None):
+	where_stmt = ""
+	if level:
+		where_stmt = "WHERE level=%(level)s"
 	frappe.db.sql("""
 		UPDATE `tabMishkah Level Enrollment` as tbl1 
 		INNER JOIN (
@@ -25,4 +27,5 @@ def update_level_enrollment_points(level_enrollment=None):
 		GROUP BY level_enrollment
 		) as tbl2 ON tbl1.name=tbl2.level_enrollment
 		SET total_level_points=tbl2.total
-""")
+		{where_stmt}
+""".format(where_stmt=where_stmt), {"level": level})
