e397ea665669f2535dcbb03f0187cb198fbe7b87
Omar Alhori <omaralhoori@gmail.com>
2024-12-11 14:42:42 +0000

    update student name

--- mishkah/mishkah/doctype/mishkah_student/mishkah_student.py
+++ mishkah/mishkah/doctype/mishkah_student/mishkah_student.py
@@ -10,3 +10,16 @@ class MishkahStudent(Document):
 	def set_fullname(self):
 		fullname = self.first_name + " " + self.middle_name + " " + self.last_name
 		self.db_set("student_name", fullname)
+		self.update_student_name_in_doctypes()
+	def update_student_name_in_doctypes(self):
+		old_doc = self.get_doc_before_save()
+		if not old_doc or old_doc.student_name == self.student_name:
+			return
+		# student groups
+		students = frappe.db.get_all("Mishkah Student Group Student", {"student": self.name}, ['name'])
+		for student in students:
+			frappe.db.set_value("Mishkah Student Group Student", student.name, "student_name", self.student_name)
+		#program enrollment
+		students = frappe.db.get_all("Mishkah Program Enrollment", {"student": self.name}, ['name'])
+		for student in students:
+			frappe.db.set_value("Mishkah Program Enrollment", student.name, "student_name", self.student_name)
